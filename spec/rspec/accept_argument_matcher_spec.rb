050b3d75053bc36145a52448bbc72aaff6660a8b
Samuel Brandão <gb.samuel@gmail.com>
2023-04-16 13:13:55 -0300

    Drop support to legacy api on accept_argument matcher

--- spec/rspec/accept_argument_matcher_spec.rb
+++ spec/rspec/accept_argument_matcher_spec.rb
@@ -5,126 +5,105 @@ require 'spec_helper'
 module RSpec
   module GraphqlMatchers
     describe 'expect(a_type).to accept_argument(field_name)' do
-      shared_examples 'accept argument' do
-        it { is_expected.to accept_argument(:id) }
+      subject(:a_type) do
+        Class.new(GraphQL::Schema::InputObject) do
+          graphql_name 'TestObject'
 
-        it 'passes when the type defines the field' do
-          expect(a_type).to accept_argument(:id)
-        end
-
-        it 'passes with the underscored argument name' do
-          expect(a_type).to accept_argument(:is_test)
-        end
-
-        it 'passes with the camel cased argument name' do
-          expect(a_type).to accept_argument(:isTest)
-        end
-
-        it 'matches a non camelized argument with the underscored argument name' do
-          expect(a_type).to accept_argument(:not_camelized)
-        end
-
-        it 'fails when the type does not define the expected field' do
-          expect { expect(a_type).to accept_argument(:ids) }
-            .to fail_with(
-              'expected TestObject to accept argument `ids` but no argument was found ' \
-              'with that name'
-            )
-        end
-
-        it 'fails with a failure message when the type does not define the field' do
-          expect { expect(a_type).to accept_argument(:ids) }
-            .to fail_with(
-              'expected TestObject to accept argument `ids` but no argument ' \
-              'was found with that name'
-            )
-        end
-
-        it 'provides a description' do
-          matcher = accept_argument(:id)
-          matcher.matches?(a_type)
-
-          expect(matcher.description).to eq('accept argument `id`')
-        end
-
-        describe '.of_type(a_type)' do
-          it 'passes when the type defines the field with correct type as ' \
-            'strings' do
-            expect(a_type).to accept_argument(:id).of_type('String')
-            expect(a_type).to accept_argument('other').of_type('ID!')
-            expect(a_type).to accept_argument('other' => 'ID!')
-          end
-
-          it 'passes when the type defines the field with correct type as ' \
-            'graphql objects' do
-            expect(a_type).to accept_argument(:id).of_type(types.String)
-            expect(a_type).to accept_argument('other').of_type('ID!')
-          end
-
-          it 'fails when the type defines a field of the wrong type' do
-            expect { expect(a_type).to accept_argument(:id).of_type('String!') }
-              .to fail_with(
-                'expected TestObject to accept argument `id` ' \
-                'of type `String!`, but it was `String`'
-              )
-
-            expect { expect(a_type).to accept_argument('other').of_type(!types.Int) }
-              .to fail_with(
-                'expected TestObject to accept argument `other` ' \
-                'of type `Int!`, but it was `ID!`'
-              )
-
-            expect { expect(a_type).to accept_argument('other' => !types.Int) }
-              .to fail_with(
-                'expected TestObject to accept argument `other` ' \
-                'of type `Int!`, but it was `ID!`'
-              )
-          end
-
-          context 'when an invalid type is passed' do
-            let(:a_type) { {} }
-
-            it 'fails with a Runtime error' do
-              expect { expect(a_type).to accept_argument(:id) }
-                .to raise_error(
-                  RuntimeError,
-                  'Invalid object {} provided to accept_argument ' \
-                  'matcher. It does not seem to be a valid GraphQL object type.'
-                )
-            end
-          end
+          argument :id, GraphQL::Types::String, required: false
+          argument :other, GraphQL::Types::ID, required: true
+          argument :is_test, GraphQL::Types::Boolean, required: false
+          argument :not_camelized, GraphQL::Types::Boolean, required: false,
+                                                            camelize: false
         end
       end
 
-      context 'using the new class-based api' do
-        subject(:a_type) do
-          Class.new(GraphQL::Schema::InputObject) do
-            graphql_name 'TestObject'
+      it { is_expected.to accept_argument(:id) }
 
-            argument :id, GraphQL::Types::String, required: false
-            argument :other, GraphQL::Types::ID, required: true
-            argument :is_test, GraphQL::Types::Boolean, required: false
-            argument :not_camelized, GraphQL::Types::Boolean, required: false,
-                                                              camelize: false
-          end
-        end
-
-        include_examples 'accept argument'
+      it 'passes when the type defines the field' do
+        expect(a_type).to accept_argument(:id)
       end
 
-      context 'with legacy DSL api' do
-        subject(:a_type) do
-          GraphQL::InputObjectType.define do
-            name 'TestObject'
+      it 'passes with the underscored argument name' do
+        expect(a_type).to accept_argument(:is_test)
+      end
 
-            argument :id, types.String
-            argument :other, !types.ID
-            argument :isTest, types.Boolean
-            argument :not_camelized, types.Boolean
-          end
+      it 'passes with the camel cased argument name' do
+        expect(a_type).to accept_argument(:isTest)
+      end
+
+      it 'matches a non camelized argument with the underscored argument name' do
+        expect(a_type).to accept_argument(:not_camelized)
+      end
+
+      it 'fails when the type does not define the expected field' do
+        expect { expect(a_type).to accept_argument(:ids) }
+          .to fail_with(
+            'expected TestObject to accept argument `ids` but no argument was found ' \
+            'with that name'
+          )
+      end
+
+      it 'fails with a failure message when the type does not define the field' do
+        expect { expect(a_type).to accept_argument(:ids) }
+          .to fail_with(
+            'expected TestObject to accept argument `ids` but no argument ' \
+            'was found with that name'
+          )
+      end
+
+      it 'provides a description' do
+        matcher = accept_argument(:id)
+        matcher.matches?(a_type)
+
+        expect(matcher.description).to eq('accept argument `id`')
+      end
+
+      describe '.of_type(a_type)' do
+        it 'passes when the type defines the field with correct type as ' \
+          'strings' do
+          expect(a_type).to accept_argument(:id).of_type('String')
+          expect(a_type).to accept_argument('other').of_type('ID!')
+          expect(a_type).to accept_argument('other' => 'ID!')
         end
 
-        include_examples 'accept argument'
+        it 'passes when the type defines the field with correct type as ' \
+          'graphql objects' do
+          expect(a_type).to accept_argument(:id).of_type(types.String)
+          expect(a_type).to accept_argument('other').of_type('ID!')
+        end
+
+        it 'fails when the type defines a field of the wrong type' do
+          expect { expect(a_type).to accept_argument(:id).of_type('String!') }
+            .to fail_with(
+              'expected TestObject to accept argument `id` ' \
+              'of type `String!`, but it was `String`'
+            )
+
+          expect { expect(a_type).to accept_argument('other').of_type(!types.Int) }
+            .to fail_with(
+              'expected TestObject to accept argument `other` ' \
+              'of type `Int!`, but it was `ID!`'
+            )
+
+          expect { expect(a_type).to accept_argument('other' => !types.Int) }
+            .to fail_with(
+              'expected TestObject to accept argument `other` ' \
+              'of type `Int!`, but it was `ID!`'
+            )
+        end
+
+        context 'when an invalid type is passed' do
+          let(:a_type) { {} }
+
+          it 'fails with a Runtime error' do
+            expect { expect(a_type).to accept_argument(:id) }
+              .to raise_error(
+                RuntimeError,
+                'Invalid object {} provided to accept_argument ' \
+                'matcher. It does not seem to be a valid GraphQL object type.'
+              )
+          end
+        end
       end
     end
   end
